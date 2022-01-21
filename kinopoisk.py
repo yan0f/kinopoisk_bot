3af0fe8b287dd00531a26db62588865741aebd35
yan0f <new@email>
2022-01-21 15:26:21 +0300

    Initial commit

---
+++ kinopoisk.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+import requests
+
+
+API_VERSION = 'v2.1'
+KINOPOISK_UNOFFICIAL_API = 'https://kinopoiskapiunofficial.tech/api/' + API_VERSION + '/'
+
+
+class Movie:
+    def __init__(self, data: dict):
+        self.kp_id = data['filmId']
+        self.name = data.get('nameEn') or data.get('nameRu') or ''
+        self.ru_name = data.get('nameRu', '')
+        self.year = data['year'].split('-')[0] if data['year'] != 'null' else None
+        self.duration = data.get('filmLength', '')
+        self.genres = [genre['genre'] for genre in data['genres']]
+        self.countries = [country['country'] for country in data['countries']]
+        self.kp_rate = data['rating'] if data['rating'] != 'null' else None
+        self.kp_url = f'https://www.kinopoisk.ru/film/{self.kp_id}/'
+        self.poster_url = data['posterUrl']
+        self.poster_preview_url = data['posterUrlPreview']
+        self.description = data.get('description', '')
+
+
+def search_for_movie(query: str) -> list[Movie]:
+    headers = {"X-API-KEY": os.getenv('KINOPOISK_API_TOKEN')}
+    request = requests.get(
+        KINOPOISK_UNOFFICIAL_API + 'films/search-by-keyword',
+        headers=headers,
+        params={"keyword": query, "page": 1}
+    )
+    request_json = json.loads(request.text)
+    if request_json.get('error'):
+        raise Exception(request_json['error'])
+    result = []
+    for film in request_json['films']:
+        result.append(Movie(film))
+    return result
---
+++ main.py
@@ -0,0 +1,82 @@
+import logging
+import os
+from uuid import uuid4
+
+from dotenv import load_dotenv
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import CallbackContext, CommandHandler, InlineQueryHandler, Updater
+
+from kinopoisk import Movie, search_for_movie
+
+load_dotenv()
+
+KINOPOISK_API_TOKEN = os.getenv('KINOPOISK_API_TOKEN')
+TELEGRAM_BOT_API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN')
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, _: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Этот бот помогает искать фильмы на Кинопоиске. '
+                              'Он работает в любом чате, просто напиши @kp_bobot в поле ввода.')
+
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def inlinequery(update: Update, _: CallbackContext) -> None:
+    """Handle the inline query."""
+    query = update.inline_query.query
+    logger.info(query)
+    if query == "":
+        return
+    movies = search_for_movie(query)
+    result = [
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            description=movie.description,
+            title=get_result_article_title(movie),
+            thumb_url=movie.poster_preview_url,
+            input_message_content=InputTextMessageContent(movie.kp_url)
+        ) for movie in movies]
+    update.inline_query.answer(result)
+
+
+def get_result_article_title(movie: Movie) -> str:
+    title = ''
+    if movie.ru_name:
+        title += f"«{movie.ru_name}» ({movie.name}, {movie.year})"
+    else:
+        title += f'{movie.name}'
+        if movie.year:
+            title += f', {movie.year}'
+    if movie.kp_rate:
+        title += f' • {movie.kp_rate}'
+    return title
+
+
+def main() -> None:
+    updater = Updater(token=TELEGRAM_BOT_API_TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(InlineQueryHandler(inlinequery))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
